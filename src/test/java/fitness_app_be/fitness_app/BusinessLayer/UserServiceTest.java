72c241b952c84ebd4babb514fea4b238f1ffa6f7
ATANAS DIMITROV <i530410@fhict.nl>
2024-09-19 10:45:50 +0200

    Update tests with return message.

--- src/test/java/fitness_app_be/fitness_app/BusinessLayer/UserServiceTest.java
+++ src/test/java/fitness_app_be/fitness_app/BusinessLayer/UserServiceTest.java
@@ -37,7 +37,6 @@ class UserServiceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        // Create mock objects
         mockUser = new User();
         mockUser.setId(1L);
         mockUser.setUsername("testUser");
@@ -51,20 +50,17 @@ class UserServiceTest {
 
     @Test
     void getAllUsers() {
-        // Arrange
+
         List<User> users = Arrays.asList(mockUser);
         when(userRepository.findAll()).thenReturn(users); // Return a list of mock users
         when(userMapper.toDto(mockUser)).thenReturn(mockUserDTO);
 
-        // Act
         List<UserDTO> userDTOList = userService.getAllUsers();
 
-        // Assert
-        assertNotNull(userDTOList);
-        assertEquals(1, userDTOList.size());
-        assertEquals("testUser", userDTOList.get(0).getUsername());
+        assertNotNull(userDTOList, "Null list of users is returned.");
+        assertEquals(1, userDTOList.size(), "Returned size users list does not match expected one.");
+        assertEquals("testUser", userDTOList.get(0).getUsername(), "Returned first username does not match expected one.");
 
-        // Verify interactions
         verify(userRepository, times(1)).findAll();
         verify(userMapper, times(1)).toDto(mockUser);
 
@@ -74,50 +70,42 @@ class UserServiceTest {
 
     @Test
     void getUserById() {
-        // Arrange
+
         when(userRepository.findById(1L)).thenReturn(Optional.of(mockUser));
         when(userMapper.toDto(mockUser)).thenReturn(mockUserDTO);
 
-        // Act
         UserDTO userDTO = userService.getUserById(1L);
 
-        // Assert
-        assertNotNull(userDTO);
-        assertEquals("testUser", userDTO.getUsername());
+        assertNotNull(userDTO, "Null user is returned.");
+        assertEquals("testUser", userDTO.getUsername(), "Incorrect username.");
 
-        // Verify interactions
         verify(userRepository, times(1)).findById(1L);
         verify(userMapper, times(1)).toDto(mockUser);
     }
 
     @Test
     void getUserById_UserNotFound() {
-        // Arrange
+
         when(userRepository.findById(1L)).thenReturn(Optional.empty());
 
-        // Act & Assert
         assertThrows(UserNotFoundException.class, () -> userService.getUserById(1L));
 
-        // Verify interactions
         verify(userRepository, times(1)).findById(1L);
         verify(userMapper, never()).toDto(any());
     }
 
     @Test
     void createUser() {
-        // Arrange
+
         when(userMapper.toEntity(mockUserDTO)).thenReturn(mockUser);
         when(userRepository.save(mockUser)).thenReturn(mockUser);
         when(userMapper.toDto(mockUser)).thenReturn(mockUserDTO);
 
-        // Act
         UserDTO savedUserDTO = userService.createUser(mockUserDTO);
 
-        // Assert
-        assertNotNull(savedUserDTO);
+        assertNotNull(savedUserDTO, "Null user is returned.");
         assertEquals("testUser", savedUserDTO.getUsername());
 
-        // Verify interactions
         verify(userMapper, times(1)).toEntity(mockUserDTO);
         verify(userRepository, times(1)).save(mockUser);
         verify(userMapper, times(1)).toDto(mockUser);
@@ -125,27 +113,23 @@ class UserServiceTest {
 
     @Test
     void deleteUser() {
-        // Act
+
         userService.deleteUser(1L);
 
-        // Verify that deleteById was called on the repository
         verify(userRepository, times(1)).deleteById(1L);
     }
 
     @Test
     void findUserByEmail() {
-        // Arrange
+
         when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(mockUser));
         when(userMapper.toDto(mockUser)).thenReturn(mockUserDTO);
 
-        // Act
         UserDTO foundUserDTO = userService.getUserByEmail("test@example.com");
 
-        // Assert
-        assertNotNull(foundUserDTO);
+        assertNotNull(foundUserDTO, "Null user is returned.");
         assertEquals("test@example.com", foundUserDTO.getEmail());
 
-        // Verify interactions
         verify(userRepository, times(1)).findByEmail("test@example.com");
         verify(userMapper, times(1)).toDto(mockUser);
     }
