944e7add0a42a35fc9204d292fc737bce512bdd2
Atanas Dimitrov <530410@student.fontys.nl>
2024-09-28 11:51:52 +0300

    Added Trainer Service components to the project

---
+++ src/main/java/fitness_app_be/fitness_app/BusinessLayer/Impl/TrainerServiceImpl.java
@@ -0,0 +1,77 @@
+package fitness_app_be.fitness_app.BusinessLayer.Impl;
+
+
+import fitness_app_be.fitness_app.BusinessLayer.TrainerService;
+import fitness_app_be.fitness_app.Domain.Trainer;
+import fitness_app_be.fitness_app.Domain.User;
+import fitness_app_be.fitness_app.ExceptionHandlingLayer.TrainerNotFoundException;
+import fitness_app_be.fitness_app.ExceptionHandlingLayer.UserNotFoundException;
+import fitness_app_be.fitness_app.MapperLayer.TrainerMapper;
+import fitness_app_be.fitness_app.MapperLayer.UserMapper;
+import fitness_app_be.fitness_app.PersistenceLayer.Entity.TrainerEntity;
+import fitness_app_be.fitness_app.PersistenceLayer.TrainerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TrainerServiceImpl implements TrainerService {
+
+    private final TrainerRepository trainerRepository;
+    private final TrainerMapper trainerMapper;
+    private final UserMapper userMapper;
+
+    @Override
+    public List<Trainer> getAllTrainers(){
+        return  trainerRepository.findAll().stream()
+                .map(trainerMapper::entityToDomain)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Trainer getTrainerById(Long id){
+        return trainerRepository.findById(id)
+                .map(trainerMapper::entityToDomain)
+                .orElseThrow(() -> new TrainerNotFoundException(id));
+    }
+
+    @Override
+    public Trainer createTrainer(Trainer trainer){
+        TrainerEntity newEntity = trainerMapper.domainToEntity(trainer);
+        TrainerEntity savedTrainerEntity = trainerRepository.save(newEntity);
+        return trainerMapper.entityToDomain(savedTrainerEntity);
+    }
+
+    @Override
+    public void deleteTrainer(Long id){
+        trainerRepository.deleteById(id);
+    }
+
+    @Override
+    public Trainer getTrainerByEmail(String email){
+        return trainerRepository.findByEmail(email)
+                .map(trainerMapper::entityToDomain)
+                .orElseThrow(() -> new TrainerNotFoundException("Trainer with email: " + email + " not found"));
+    }
+
+    @Override
+    public List<Trainer> searchTrainersByPartialUsername(String partialUsername){
+        return trainerRepository.findByUsernameContainingIgnoreCase(partialUsername).stream()
+                .map(trainerMapper::entityToDomain)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Trainer updateTrainer(Trainer trainer){
+        TrainerEntity existingTrainerEntity = trainerRepository.findByEmail(trainer.getEmail())
+                .orElseThrow(() -> new TrainerNotFoundException("Trainer with email " + trainer.getEmail() + " not found"));
+
+        existingTrainerEntity.setExpertise(trainer.getExpertise());
+        TrainerEntity updatedTrainerEntity =  trainerRepository.save(existingTrainerEntity);
+        return trainerMapper.entityToDomain(updatedTrainerEntity);
+
+    }
+}
--- src/main/java/fitness_app_be/fitness_app/BusinessLayer/TrainerService.java
+++ src/main/java/fitness_app_be/fitness_app/BusinessLayer/TrainerService.java
@@ -1,4 +1,16 @@
 package fitness_app_be.fitness_app.BusinessLayer;
 
+import fitness_app_be.fitness_app.Domain.Trainer;
+import fitness_app_be.fitness_app.Domain.User;
+
+import java.util.List;
+
 public interface TrainerService {
+    List<Trainer> getAllTrainers();
+    Trainer getTrainerById(Long id);
+    Trainer createTrainer(Trainer trainer);
+    void deleteTrainer(Long id);
+    Trainer getTrainerByEmail(String email);
+    List<Trainer> searchTrainersByPartialUsername(String partialUsername);
+    Trainer updateTrainer(Trainer trainer);
 }
