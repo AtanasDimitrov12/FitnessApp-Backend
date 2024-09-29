b2f55052c8ed4f96d0ceda4ff4405cfa68a22f76
Atanas Dimitrov <530410@student.fontys.nl>
2024-09-29 10:09:04 +0300

    Added test class for TrainerServiceImpl

---
+++ src/test/java/fitness_app_be/fitness_app/BusinessLayer/Impl/TrainerServiceImplTest.java
@@ -0,0 +1,41 @@
+package fitness_app_be.fitness_app.BusinessLayer.Impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainerServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllTrainers() {
+    }
+
+    @Test
+    void getTrainerById() {
+    }
+
+    @Test
+    void createTrainer() {
+    }
+
+    @Test
+    void deleteTrainer() {
+    }
+
+    @Test
+    void getTrainerByEmail() {
+    }
+
+    @Test
+    void searchTrainersByPartialUsername() {
+    }
+
+    @Test
+    void updateTrainer() {
+    }
+}
