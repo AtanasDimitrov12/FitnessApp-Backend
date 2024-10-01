e3be1c898faa9b99d23d79f9deae4761c9aa0cde
Atanas Dimitrov <530410@student.fontys.nl>
2024-10-01 09:44:40 +0300

    Refactor all workout classes to work with FakeRepository

--- src/main/java/fitness_app_be/fitness_app/Persistence/WorkoutRepository.java
+++ src/main/java/fitness_app_be/fitness_app/Persistence/WorkoutRepository.java
@@ -2,14 +2,23 @@ package fitness_app_be.fitness_app.Persistence;
 
 import fitness_app_be.fitness_app.Domain.Enums.Exercises;
 import fitness_app_be.fitness_app.Domain.Workout;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+public interface WorkoutRepository {
+
+    boolean exists(long id);
+
+    List<Workout> getAll();
+
+    Workout create(Workout workout);
+
+    Workout update(Workout workout);
+
+    void delete(long workoutId);
+
+    Optional<Workout> getWorkoutById(long workoutId);
 
     List<Workout> findByNameContainingIgnoreCase(String name);
 
