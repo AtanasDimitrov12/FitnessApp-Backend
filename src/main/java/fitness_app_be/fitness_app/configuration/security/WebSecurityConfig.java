76a8c5894b289491378a359f57e44d040fc4315d
Atanas Dimitrov <apdimitrov12@gmail.com>
2025-02-12 12:52:47 +0100

    Update db connection to work with hosted db

--- src/main/java/fitness_app_be/fitness_app/configuration/security/WebSecurityConfig.java
+++ src/main/java/fitness_app_be/fitness_app/configuration/security/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174")); // Frontend origin
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://my-fitness-guru.netlify.app")); // Frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
